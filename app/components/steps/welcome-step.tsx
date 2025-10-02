2e5641b7a1cb71aa0a68481ff6dd8c955db2eeef
Soma Somorjai <somafet@gmail.com>
2025-10-02 11:02:12 +0200

    Welcome step animations

--- app/components/steps/welcome-step.tsx
+++ app/components/steps/welcome-step.tsx
@@ -9,19 +9,24 @@ export function WelcomeStep({ className }: React.ComponentProps<"div">) {
     <div className={cn("flex flex-col gap-6", className)}>
       <div className="flex flex-col items-center gap-2 text-center">
         <a href="#" className="flex flex-col items-center gap-2 font-medium">
-          <div className="flex size-32 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-12" />
+          <div className="flex size-32 items-center justify-center rounded-md relative">
+            <GalleryVerticalEnd className="size-12 animate-logo absolute z-10" />
           </div>
           <span className="sr-only">Acme Inc.</span>
         </a>
-        <h1 className="text-5xl font-bold mb-4">Welcome to Linear</h1>
-        <p className="text-muted-foreground w-[620px] max-w-[90vw] mb-8">
+        <h1 className="text-5xl font-bold mb-4 animate-content">
+          Welcome to Linear
+        </h1>
+        <p className="text-muted-foreground w-[620px] max-w-[90vw] mb-8 animate-content">
           Linear is a purpose-built system for developing products.
           <br />
           Streamline issues, projects, and product roadmaps.
         </p>
 
-        <Button className="w-[336px] h-12" onClick={() => next()}>
+        <Button
+          className="w-[336px] h-12 animate-content delay-[1.4s]! duration-[0.5s]!"
+          onClick={() => next()}
+        >
           Get started
         </Button>
       </div>
